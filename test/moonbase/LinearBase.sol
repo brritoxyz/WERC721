c7652cdb8ad9734f8c497595cd8af451bdcb8c0b
kphed <kpcrypto69@gmail.com>
2023-03-21 08:51:55 -0400

    Deployer and whitelist MoonRouter

--- test/moonbase/LinearBase.sol
+++ test/moonbase/LinearBase.sol
@@ -3,21 +3,28 @@ pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
 
+import {LinearCurve} from "src/bonding-curves/LinearCurve.sol";
 import {PairEnumerableETH} from "sudoswap/PairEnumerableETH.sol";
 import {PairMissingEnumerableETH} from "sudoswap/PairMissingEnumerableETH.sol";
 import {PairEnumerableERC20} from "sudoswap/PairEnumerableERC20.sol";
 import {PairMissingEnumerableERC20} from "sudoswap/PairMissingEnumerableERC20.sol";
 import {PairFactory} from "sudoswap/PairFactory.sol";
+import {RouterWithRoyalties} from "src/MoonRouter.sol";
 
 contract LinearBase is Test {
     // 0.40%
     uint256 private constant PROTOCOL_FEE_MULTIPLIER = 0.004e18;
 
+    // Unchanged SudoSwap contracts
+    LinearCurve private immutable linearCurve = new LinearCurve();
     PairFactory private immutable pairFactory;
     PairEnumerableETH private immutable pairEnumerableETH;
     PairMissingEnumerableETH private immutable pairMissingEnumerableETH;
     PairEnumerableERC20 private immutable pairEnumerableERC20;
     PairMissingEnumerableERC20 private immutable pairMissingEnumerableERC20;
+
+   // Moonbase
+    RouterWithRoyalties private immutable moonRouter;
 
     constructor() {
         pairEnumerableETH = new PairEnumerableETH();
@@ -34,5 +41,14 @@ contract LinearBase is Test {
             payable(address(this)),
             PROTOCOL_FEE_MULTIPLIER
         );
+
+        // Whitelist bonding curve
+        pairFactory.setBondingCurveAllowed(linearCurve, true);
+
+        // Deploy MoonRouter
+        moonRouter = new RouterWithRoyalties(pairFactory);
+
+        // Whitelist MoonRouter
+        pairFactory.setRouterAllowed(moonRouter, true);
     }
 }
