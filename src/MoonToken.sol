6eceabdb17316fac82c90fb89f5438ec985f1b94
kphed <kpcrypto69@gmail.com>
2023-03-24 11:41:18 -0400

    Add minting-related methods

--- src/MoonToken.sol
+++ src/MoonToken.sol
@@ -6,13 +6,22 @@ import {Owned} from "solmate/auth/Owned.sol";
 
 contract MoonToken is Owned, ERC20("MoonBase", "MOON", 18) {
     address public router;
+    mapping(address user => uint256 amount) public mintable;
 
     event SetRouter(address);
+    event IncreaseMintable(address, uint256);
 
+    error Unauthorized();
     error InvalidAddress();
+    error InvalidAmount();
 
     constructor(address _owner) Owned(_owner) {
         if (_owner == address(0)) revert InvalidAddress();
+    }
+
+    modifier onlyRouter() {
+        if (msg.sender != router) revert Unauthorized();
+        _;
     }
 
     function setRouter(address _router) external onlyOwner {
@@ -21,5 +30,24 @@ contract MoonToken is Owned, ERC20("MoonBase", "MOON", 18) {
         router = _router;
 
         emit SetRouter(_router);
+    }
+
+    function increaseMintable(address to, uint256 amount) external onlyRouter {
+        if (to == address(0)) revert InvalidAddress();
+        if (amount == 0) revert InvalidAmount();
+
+        mintable[to] += amount;
+
+        emit IncreaseMintable(to, amount);
+    }
+
+    function mint() external {
+        uint256 amount = mintable[msg.sender];
+
+        if (amount == 0) revert InvalidAmount();
+
+        mintable[msg.sender] = 0;
+
+        _mint(msg.sender, amount);
     }
 }
