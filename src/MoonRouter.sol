e96900f71e7bffe5add08e714f24a6f56b8c087f
kphed <kpcrypto69@gmail.com>
2023-03-31 16:38:14 -0400

    Remove Sudoswap contracts

--- src/MoonRouter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.19;
-
-import {RouterWithRoyalties} from "sudoswap/RouterWithRoyalties.sol";
-import {PairFactory} from "sudoswap/PairFactory.sol";
-
-contract MoonRouter is RouterWithRoyalties {
-    constructor(PairFactory _factory) RouterWithRoyalties(_factory) {}
-}
--- test/sudoswap/interfaces/IMintable.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
-
-interface IMintable {
-    function mint(address a, uint256 amount) external;
-}
