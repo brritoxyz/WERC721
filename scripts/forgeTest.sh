a92d85e5d38a4f4eaf238aed320b6840496e161f
kphed <kpcrypto69@gmail.com>
2023-04-03 05:45:44 -0400

    Add setCollectionRoyalties tests

--- scripts/forgeTest.sh
+++ scripts/forgeTest.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Load environment variables
 source $SCRIPT_DIR/loadEnv.sh
 
-forge snapshot --match-path "test/moonbase/*" --no-match-path "test/sudoswap/*" --fork-url $LOCAL_PROVIDER "$@"
+forge snapshot --fork-url $LOCAL_PROVIDER "$@"
--- test/MoonPool.t.sol
+++ test/MoonPool.t.sol
@@ -13,9 +13,31 @@ contract MoonPoolTest is Test, ERC721TokenReceiver {
         0x2aE6B0630EBb4D155C6e04fCB16840FFA77760AA;
 
     MoonPool private immutable pool;
+    uint96 private immutable bpsBase;
+    uint80 private immutable maxCollectionRoyalties;
+    uint80 private immutable maxProtocolFees;
 
     // NFT IDs that are owned by the impersonated/pranked address
     uint256[] private initialNftIds = [0, 2, 7];
+
+    event SetCollectionRoyalties(address indexed recipient, uint96 bps);
+    event SetProtocolFees(address indexed recipient, uint96 bps);
+    event List(address indexed seller, uint256 indexed id, uint96 price);
+    event ListMany(address indexed seller, uint256[] ids, uint96[] prices);
+    event Buy(
+        address indexed buyer,
+        address indexed seller,
+        uint256 indexed id,
+        uint96 price,
+        uint256 totalFees
+    );
+    event BuyMany(
+        address indexed buyer,
+        uint256[] ids,
+        uint256 totalPrice,
+        uint256 totalCollectionRoyalties,
+        uint256 totalProtocolFees
+    );
 
     constructor() {
         vm.startPrank(AZUKI_OWNER);
@@ -40,5 +62,77 @@ contract MoonPoolTest is Test, ERC721TokenReceiver {
         vm.stopPrank();
 
         pool = new MoonPool(address(this), AZUKI);
+        bpsBase = pool.BPS_BASE();
+        maxCollectionRoyalties = pool.MAX_COLLECTION_ROYALTIES();
+        maxProtocolFees = pool.MAX_PROTOCOL_FEES();
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        setCollectionRoyalties
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotSetCollectionRoyaltiesUnauthorized() external {
+        vm.prank(address(0));
+        vm.expectRevert(bytes("UNAUTHORIZED"));
+
+        pool.setCollectionRoyalties(address(this), 1000);
+    }
+
+    function testCannotSetCollectionRoyaltiesRecipientInvalidAddress()
+        external
+    {
+        assertEq(address(this), pool.owner());
+
+        vm.expectRevert(MoonPool.InvalidAddress.selector);
+
+        pool.setCollectionRoyalties(address(0), 1000);
+    }
+
+    function testCannotSetCollectionRoyaltiesBpsExceedsBase() external {
+        assertEq(address(this), pool.owner());
+
+        address recipient = address(this);
+        uint96 bps = bpsBase + 1;
+
+        vm.expectRevert(MoonPool.InvalidNumber.selector);
+
+        pool.setCollectionRoyalties(recipient, bps);
+    }
+
+    function testCannotSetCollectionRoyaltiesBpsExceedsMax() external {
+        assertEq(address(this), pool.owner());
+
+        address recipient = address(this);
+        uint96 bps = maxCollectionRoyalties + 1;
+
+        vm.expectRevert(MoonPool.InvalidNumber.selector);
+
+        pool.setCollectionRoyalties(recipient, bps);
+    }
+
+    function testSetCollectionRoyalties(uint96 bps) external {
+        vm.assume(bps <= bpsBase);
+        vm.assume(bps <= maxCollectionRoyalties);
+
+        assertEq(address(this), pool.owner());
+
+        address recipient = address(this);
+
+        (address recipientBefore, uint96 bpsBefore) = pool
+            .collectionRoyalties();
+
+        assertEq(address(0), recipientBefore);
+        assertEq(0, bpsBefore);
+
+        vm.expectEmit(true, false, false, true, address(pool));
+
+        emit SetCollectionRoyalties(recipient, bps);
+
+        pool.setCollectionRoyalties(recipient, bps);
+
+        (address recipientAfter, uint96 bpsAfter) = pool.collectionRoyalties();
+
+        assertEq(recipient, recipientAfter);
+        assertEq(bps, bpsAfter);
     }
 }
