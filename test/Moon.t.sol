f7eb4209ec173bc749c35fa6033f0ad45906f0bf
kphed <kpcrypto69@gmail.com>
2023-04-05 07:01:40 -0400

    Move testAccounts array to base contract

--- test/Moon.t.sol
+++ test/Moon.t.sol
@@ -10,12 +10,6 @@ contract MoonTest is Test, Moonbase {
     event SetFactory(address indexed factory);
     event AddMinter(address indexed factory, address indexed minter);
 
-    address[3] private testAccounts = [
-        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
-        0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
-        0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
-    ];
-
     /*//////////////////////////////////////////////////////////////
                                 setFactory
     //////////////////////////////////////////////////////////////*/
--- test/Moonbase.sol
+++ test/Moonbase.sol
@@ -14,6 +14,12 @@ contract Moonbase is Test {
     Moon internal immutable moon;
     MoonBookFactory internal immutable factory;
 
+    address[3] internal testAccounts = [
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+        0x70997970C51812dc3A010C7d01b50e0d17dc79C8,
+        0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+    ];
+
     constructor() {
         MoonDeployment deployment = new MoonDeployment(address(this));
         moon = deployment.moon();
