ac11b5102030a799c68439557c0eb556e500896d
kphed <kpcrypto69@gmail.com>
2023-04-19 12:16:53 -0400

    Add tests for setMoonStaker

--- test/MoonV2.t.sol
+++ test/MoonV2.t.sol
@@ -7,22 +7,59 @@ import {Moon} from "src/MoonV2.sol";
 import {MoonStaker} from "src/MoonStaker.sol";
 
 contract MoonTest is Test {
+    bytes private constant UNAUTHORIZED_ERROR = bytes("UNAUTHORIZED");
+
     Moon private immutable moon;
     MoonStaker private immutable moonStaker;
     address private immutable moonAddr;
 
-    event StakeETH(address indexed msgSender, uint256 assets, uint256 shares);
+    event SetMoonStaker(address indexed msgSender, MoonStaker moonStaker);
+    event StakeETH(
+        address indexed msgSender,
+        uint256 balance,
+        uint256 assets,
+        uint256 shares
+    );
     event DepositETH(address indexed msgSender, uint256 msgValue);
 
     constructor() {
         moon = new Moon();
 
-        moonStaker = new MoonStaker(address(moon));
-
         // To avoid redundant casting
         moonAddr = address(moon);
 
+        moonStaker = new MoonStaker(moonAddr);
+
         moon.setMoonStaker(moonStaker);
+    }
+
+    function testCannotSetMoonStakerUnauthorized() external {
+        vm.prank(address(0));
+        vm.expectRevert(UNAUTHORIZED_ERROR);
+
+        moon.setMoonStaker(moonStaker);
+    }
+
+    function testCannotSetMoonStakerInvalidAddress() external {
+        vm.expectRevert(Moon.InvalidAddress.selector);
+
+        moon.setMoonStaker(MoonStaker(address(0)));
+    }
+
+    function testSetMoonStaker() external {
+        address msgSender = address(this);
+        MoonStaker newMoonStaker = new MoonStaker(moonAddr);
+
+        assertEq(msgSender, moon.owner());
+        assertFalse(address(newMoonStaker) == address(moon.moonStaker()));
+
+        vm.expectEmit(true, false, false, true, moonAddr);
+
+        emit SetMoonStaker(msgSender, newMoonStaker);
+
+        moon.setMoonStaker(newMoonStaker);
+
+        assertEq(address(newMoonStaker), address(moon.moonStaker()));
     }
 
     function testCannotDepositETHInvalidAmount() external {
