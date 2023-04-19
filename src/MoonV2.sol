772c65ca28fed16969df3a1170f05681402c65b8
kphed <kpcrypto69@gmail.com>
2023-04-19 10:18:33 -0400

    Update depositETH to stake ETH with Lido

--- src/MoonV2.sol
+++ src/MoonV2.sol
@@ -3,10 +3,22 @@ pragma solidity 0.8.19;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
+interface ILido {
+    function submit(address _referral) external payable returns (uint256);
+}
+
 contract Moon is ERC20("Redeemable Token", "MOON", 18) {
+    // Lido stETH proxy contract address
+    ILido public constant LIDO =
+        ILido(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+
     address payable public immutable protocolTeam;
 
-    event DepositETH(address indexed depositor, uint256 amount);
+    event DepositETH(
+        address indexed msgSender,
+        uint256 msgValue,
+        uint256 staked
+    );
 
     error InvalidAddress();
     error InvalidAmount();
@@ -18,14 +30,17 @@ contract Moon is ERC20("Redeemable Token", "MOON", 18) {
     }
 
     /**
-     * @notice Deposit ETH for MOON
+     * @notice Deposit ETH, receive MOON
      */
     function depositETH() external payable {
         if (msg.value == 0) revert InvalidAmount();
 
-        // Mint MOON for msg.sender, equal to the ETH amount deposited
-        _mint(msg.sender, msg.value);
+        // Stake ETH in Lido
+        uint256 staked = LIDO.submit{value: msg.value}(address(0));
 
-        emit DepositETH(msg.sender, msg.value);
+        // Mint MOON for msg.sender, equal to the stETH received
+        _mint(msg.sender, staked);
+
+        emit DepositETH(msg.sender, msg.value, staked);
     }
 }
