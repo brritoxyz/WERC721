b1bbbc44ed304a95b23ba542e3b3559cd59542b6
kphed <kpcrypto69@gmail.com>
2023-04-19 11:23:46 -0400

    Deposit stETH into Instadapp, and update tests

--- src/MoonV2.sol
+++ src/MoonV2.sol
@@ -4,16 +4,29 @@ pragma solidity 0.8.19;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
+interface UserModule {
+    function deposit(uint256, address) external returns (uint256);
+}
+
 contract Moon is ERC20("Redeemable Token", "MOON", 18) {
     using SafeTransferLib for address payable;
+    using SafeTransferLib for ERC20;
 
     // Lido stETH proxy contract address
     address payable public constant LIDO =
         payable(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
 
+    // Instadapp proxy contract address
+    UserModule public constant INSTADAPP =
+        UserModule(0xA0D3707c569ff8C87FA923d3823eC5D81c98Be78);
+
     address public immutable protocolTeam;
 
-    event DepositETH(address indexed msgSender, uint256 msgValue);
+    event DepositETH(
+        address indexed msgSender,
+        uint256 msgValue,
+        uint256 shares
+    );
 
     error InvalidAddress();
     error InvalidAmount();
@@ -22,20 +35,30 @@ contract Moon is ERC20("Redeemable Token", "MOON", 18) {
         if (_protocolTeam == address(0)) revert InvalidAddress();
 
         protocolTeam = _protocolTeam;
+
+        // Enables Instadapp to transfer stETH on our behalf when depositing
+        ERC20(LIDO).safeApprove(address(INSTADAPP), type(uint256).max);
     }
 
     /**
      * @notice Deposit ETH, receive MOON
+     * @return shares  uint256  Instadapp shares minted from ETH deposit
      */
-    function depositETH() external payable {
+    function depositETH() external payable returns (uint256 shares) {
         if (msg.value == 0) revert InvalidAmount();
 
         // Stake ETH in Lido
         LIDO.safeTransferETH(msg.value);
 
-        // Mint MOON for msg.sender, equal to the ETH received
-        _mint(msg.sender, msg.value);
+        // Deposit stETH into Instadapp to maximize returns
+        shares = INSTADAPP.deposit(
+            ERC20(LIDO).balanceOf(address(this)),
+            address(this)
+        );
 
-        emit DepositETH(msg.sender, msg.value);
+        // Mint MOON for msg.sender, equal to the Instadapp shares received
+        _mint(msg.sender, shares);
+
+        emit DepositETH(msg.sender, msg.value, shares);
     }
 }
--- test/MoonV2.t.sol
+++ test/MoonV2.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
-
+import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Moon} from "src/MoonV2.sol";
 
 interface ILido {
@@ -13,12 +13,25 @@ interface ILido {
     function balanceOf(address) external view returns (uint256);
 }
 
+interface IUserModule {
+    function balanceOf(address) external view returns (uint256);
+
+    function convertToShares(uint256) external view returns (uint256);
+
+    function convertToAssets(uint256) external view returns (uint256);
+}
+
 contract MoonTest is Test {
     Moon private immutable moon;
     address private immutable moonAddr;
     ILido private immutable lido;
+    IUserModule private immutable instadapp;
 
-    event DepositETH(address indexed msgSender, uint256 msgValue);
+    event DepositETH(
+        address indexed msgSender,
+        uint256 msgValue,
+        uint256 shares
+    );
 
     constructor() {
         moon = new Moon(address(this));
@@ -28,6 +41,23 @@ contract MoonTest is Test {
 
         // Lido with a different contract interface for testing only
         lido = ILido(address(moon.LIDO()));
+
+        // Instadapp with an ERC20 interface
+        instadapp = IUserModule(address(moon.INSTADAPP()));
+    }
+
+    function _toStEth(uint256 ethAmount) private view returns (uint256) {
+        return lido.getPooledEthByShares(lido.getSharesByPooledEth(ethAmount));
+    }
+
+    function _convertToShares(
+        uint256 ethAmount
+    ) private view returns (uint256) {
+        return instadapp.convertToShares(_toStEth(ethAmount));
+    }
+
+    function _convertToAssets(uint256 shares) private view returns (uint256) {
+        return instadapp.convertToAssets(shares);
     }
 
     function testCannotDepositETHInvalidAmount() external {
@@ -41,21 +71,22 @@ contract MoonTest is Test {
         vm.assume(amount != 0);
 
         uint256 ethAmount = uint256(amount) * 1 ether;
+        uint256 expectedShares = _convertToShares(ethAmount);
 
         vm.deal(msgSender, ethAmount);
         vm.prank(msgSender);
-        vm.expectEmit(true, false, false, true, address(moon));
+        vm.expectEmit(true, false, false, true, moonAddr);
 
-        emit DepositETH(msgSender, ethAmount);
+        emit DepositETH(msgSender, ethAmount, expectedShares);
 
-        moon.depositETH{value: ethAmount}();
+        uint256 shares = moon.depositETH{value: ethAmount}();
+        uint256 assets = _convertToAssets(expectedShares);
 
-        // Calculate the amount of stETH received from the deposited ETH
-        uint256 poolEthByShares = lido.getPooledEthByShares(
-            lido.getSharesByPooledEth(ethAmount)
-        );
+        assertEq(expectedShares, instadapp.balanceOf(moonAddr));
+        assertEq(expectedShares, moon.balanceOf(msgSender));
+        assertEq(expectedShares, shares);
 
-        assertEq(poolEthByShares, lido.balanceOf(address(moon)));
-        assertEq(ethAmount, moon.balanceOf(msgSender));
+        // We deduct 1 from the stETH amount (Instadapp does this to account for rounding)
+        assertEq(_toStEth(ethAmount) - 1, assets);
     }
 }
