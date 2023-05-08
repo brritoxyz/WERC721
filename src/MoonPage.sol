2c5e326b3248b79cd7ce3a33c9bc58e71a6ecfd5
kphed <kpcrypto69@gmail.com>
2023-05-08 06:11:29 -0400

    Consolidate MoonList and MoonPage

--- src/MoonPage.sol
+++ src/MoonPage.sol
@@ -2,22 +2,45 @@
 pragma solidity 0.8.19;
 
 import {Initializable} from "openzeppelin/proxy/utils/Initializable.sol";
-import {Owned} from "solmate/auth/Owned.sol";
-import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
 import {ERC721, ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
-import {ERC1155, ERC1155TokenReceiver} from "src/base/MoonERC1155.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {Owned} from "src/base/Owned.sol";
+import {ReentrancyGuard} from "src/base/ReentrancyGuard.sol";
+import {ERC1155, ERC1155TokenReceiver} from "src/base/ERC1155.sol";
 
 contract MoonPage is
     Initializable,
-    ReentrancyGuard,
     Owned,
+    ReentrancyGuard,
     ERC721TokenReceiver,
     ERC1155
 {
+    using SafeTransferLib for address payable;
+
+    struct Listing {
+        address seller;
+        uint96 price;
+    }
+
     ERC721 public collection;
+
+    mapping(uint256 => Listing) public listings;
+
+    event List(uint256 indexed id, address indexed seller, uint96 price);
+    event Edit(uint256 indexed id, address indexed seller, uint96 newPrice);
+    event Cancel(uint256 indexed id, address indexed seller);
+    event Buy(
+        uint256 indexed id,
+        address indexed buyer,
+        address indexed seller,
+        uint96 price
+    );
 
     error Zero();
     error Invalid();
+    error Unauthorized();
+    error Nonexistent();
+    error Insufficient();
 
     constructor() Owned(msg.sender) {
         // Disable initialization on the implementation contract
@@ -35,6 +58,7 @@ contract MoonPage is
     ) external initializer {
         owner = _owner;
         collection = _collection;
+        locked = 1;
     }
 
     function setURI(string memory newuri) external onlyOwner {
@@ -74,5 +98,81 @@ contract MoonPage is
 
         // Transfer the NFT to the recipient
         collection.safeTransferFrom(address(this), recipient, id);
+    }
+
+    /**
+     * @notice Create a listing
+     * @param  id     uint256  Collection token ID
+     * @param  price  uint96   Price
+     */
+    function list(uint256 id, uint96 price) external {
+        // Reverts if msg.sender does not have the token
+        if (ownerOf[id] != msg.sender) revert Unauthorized();
+
+        // Update token owner to this contract to prevent double-listing
+        ownerOf[id] = address(this);
+
+        // Set the listing
+        listings[id] = Listing(msg.sender, price);
+
+        emit List(id, msg.sender, price);
+    }
+
+    /**
+     * @notice Edit a listing
+     * @param  id        uint256  Collection token ID
+     * @param  newPrice  uint96   New price
+     */
+    function edit(uint256 id, uint96 newPrice) external {
+        Listing storage listing = listings[id];
+
+        // Reverts if msg.sender is not the seller
+        if (listing.seller != msg.sender) revert Unauthorized();
+
+        // Update the listing price
+        listing.price = newPrice;
+
+        emit Edit(id, msg.sender, newPrice);
+    }
+
+    /**
+     * @notice Cancel a listing
+     * @param  id  uint256  Collection token ID
+     */
+    function cancel(uint256 id) external {
+        // Reverts if msg.sender is not the seller
+        if (listings[id].seller != msg.sender) revert Unauthorized();
+
+        // Delete listing prior to returning the token
+        delete listings[id];
+
+        ownerOf[id] = msg.sender;
+
+        emit Cancel(id, msg.sender);
+    }
+
+    /**
+     * @notice Cancel a listing
+     * @param  id  uint256  Collection token ID
+     */
+    function buy(uint256 id) external payable nonReentrant {
+        Listing memory listing = listings[id];
+
+        // Reverts if the listing does not exist
+        if (listing.seller == address(0)) revert Nonexistent();
+
+        // Reverts if the msg.value does not cover the price
+        if (msg.value != listing.price) revert Insufficient();
+
+        // Delete listing prior to returning the token
+        delete listings[id];
+
+        // Set the new token owner to the buyer
+        ownerOf[id] = msg.sender;
+
+        // Transfer ETH to the seller
+        payable(listing.seller).safeTransferETH(msg.value);
+
+        emit Buy(id, msg.sender, listing.seller, listing.price);
     }
 }
