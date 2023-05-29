6a275a1a05530b6a75b37a96a67314f0c5b07544
kphed <kpcrypto69@gmail.com>
2023-05-29 11:03:54 -0400

    Add FrontPageERC721

---
+++ src/FrontPageERC721.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Ownable} from "solady/auth/Ownable.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {LibString} from "solady/utils/LibString.sol";
+
+contract FrontPageERC721 is Ownable, ERC721 {
+    string public baseURI;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        _initializeOwner(msg.sender);
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        baseURI = baseURI_;
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        return string(abi.encodePacked(baseURI, LibString.toString(id)));
+    }
+}
