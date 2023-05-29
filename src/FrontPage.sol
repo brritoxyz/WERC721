745dd3a171724ddc80edb70cfde4db4310ca2a0c
kphed <kpcrypto69@gmail.com>
2023-05-29 12:21:54 -0400

    Add batch mint and redeem methods

--- src/FrontPage.sol
+++ src/FrontPage.sol
@@ -75,16 +75,73 @@ contract FrontPage is PageToken {
     }
 
     /**
-     * @notice Mint the FrontPage token representing the redeemable NFT
+     * @notice Mint multiple FrontPage tokens representing the redeemable NFTs
+     * @param  quantity  uint256  Number of FPTs to mint
+     */
+    function batchMint(uint256 quantity) external payable {
+        // Revert if the mint quantity is zero
+        if (quantity == 0) revert Zero();
+
+        // Revert if the value sent is less than the mint price for the quantity
+        if (msg.value != uint256(mintPrice) * quantity)
+            revert InsufficientFunds();
+
+        // Initial value of the loop iterator variable and next token ID
+        uint256 i = totalSupply;
+
+        // Update totalSupply to reflect the additional tokens to be minted
+        uint256 totalSupplyAfter = (totalSupply += quantity);
+
+        // Revert if the max NFT supply has been or will be exceeded post-mint
+        if (totalSupplyAfter > uint256(maxSupply)) revert Soldout();
+
+        for (i; i < totalSupplyAfter; ) {
+            // Set the owner of the token ID to the minter
+            ownerOf[i] = msg.sender;
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /**
+     * @notice Redeem the FrontPage token for the underlying NFT
+     * @param  id  uint256  FrontPage token ID
      */
     function redeem(uint256 id) external {
         if (ownerOf[id] != msg.sender) revert Unauthorized();
 
-        // Transfer the FrontPage token to self, effectively burning it
-        // without affecting the total supply
+        // Transfer the FrontPage token to self, burning it without affecting
+        // the total supply (to reduce gas)
         ownerOf[id] = address(this);
 
         // Mint the NFT for msg.sender with the same ID as the FrontPage token
         collection.mint(msg.sender, id);
+    }
+
+    /**
+     * @notice Redeem the FrontPage tokens for the underlying NFTs
+     * @param  ids  uint256[]  FrontPage token IDs
+     */
+    function batchRedeem(uint256[] calldata ids) external {
+        uint256 id;
+
+        for (uint256 i; i < ids.length; ) {
+            id = ids[i];
+
+            if (ownerOf[id] != msg.sender) revert Unauthorized();
+
+            // Transfer the FrontPage token to self, burning it without affecting
+            // the total supply (to reduce gas)
+            ownerOf[id] = address(this);
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        // Mint the NFTs for msg.sender with the same IDs as the FrontPage tokens
+        collection.batchMint(msg.sender, ids);
     }
 }
--- src/FrontPageERC721.sol
+++ src/FrontPageERC721.sol
@@ -39,4 +39,18 @@ contract FrontPageERC721 is Ownable, ERC721 {
 
         _mint(to, id);
     }
+
+    function batchMint(address to, uint256[] calldata ids) external {
+        // Users must redeem through the FrontPage contract, the only authorized caller of this method
+        // The FrontPage contract burns the token ID prior to minting the NFT, preventing reuse
+        if (msg.sender != frontPage) revert Unauthorized();
+
+        for (uint256 i; i < ids.length; ) {
+            _mint(to, ids[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
 }
