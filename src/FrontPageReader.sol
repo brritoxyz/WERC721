34bb0c10a4f6f4a3a47065ea3e57cf1e572420e8
kphed <kpcrypto69@gmail.com>
2023-06-16 06:39:55 -0400

    Add listing retrieval methods

--- src/FrontPageReader.sol
+++ src/FrontPageReader.sol
@@ -4,9 +4,18 @@ pragma solidity 0.8.20;
 import {DynamicBufferLib} from "solady/utils/DynamicBufferLib.sol";
 
 interface FrontPage {
-    function ownerOf(uint256 id) external view returns (address);
+    struct Listing {
+        // Seller address
+        address payable seller;
+        // Adequate for 79m ether
+        uint96 price;
+    }
 
     function nextId() external view returns (uint256);
+
+    function ownerOf(uint256 id) external view returns (address);
+
+    function listings(uint256 id) external view returns (Listing memory);
 }
 
 contract FrontPageReader {
@@ -39,6 +48,43 @@ contract FrontPageReader {
             if (frontPage.ownerOf(id) == owner) {
                 ids.append(abi.encode(id));
             }
+
+            unchecked {
+                ++id;
+            }
+        }
+    }
+
+    function allListings()
+        external
+        view
+        returns (DynamicBufferLib.DynamicBuffer memory ids)
+    {
+        uint256 maxId = frontPage.nextId();
+
+        for (uint256 id = 1; id < maxId; ) {
+            unchecked {
+                ++id;
+            }
+
+            FrontPage.Listing memory listing = frontPage.listings(id);
+
+            if (listing.seller == address(0)) continue;
+
+            ids.append(abi.encode(id, listing.seller, listing.price));
+        }
+    }
+
+    function listingsByAccount(
+        address account
+    ) external view returns (DynamicBufferLib.DynamicBuffer memory ids) {
+        uint256 maxId = frontPage.nextId();
+
+        for (uint256 id = 1; id < maxId; ) {
+            FrontPage.Listing memory listing = frontPage.listings(id);
+
+            if (listing.seller == account)
+                ids.append(abi.encode(id, listing.price));
 
             unchecked {
                 ++id;
