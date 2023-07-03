bfe9fbfeb5b2cd4229d2cc628b3c35511fa1f8f4
kphed <kpcrypto69@gmail.com>
2023-07-03 10:56:53 -0400

    Add PageToken.transferFrom and PageToken.batchTransferFrom tests

--- test/PageToken.t.sol
+++ test/PageToken.t.sol
@@ -107,21 +107,25 @@ contract PageTokenTest is Test {
 
     function testTransferFuzz(uint256 id, address from, address to) external {
         vm.assume(from != address(0));
+        vm.assume(from != to);
 
         pageToken.setOwnerOf(id, from);
 
         assertEq(1, pageToken.balanceOf(from, id));
         assertEq(0, pageToken.balanceOf(to, id));
 
+        bool toIsUnsafe = to == address(0);
+
         vm.prank(from);
 
-        if (to == address(0))
-            vm.expectRevert(PageToken.UnsafeRecipient.selector);
+        if (toIsUnsafe) vm.expectRevert(PageToken.UnsafeRecipient.selector);
 
         pageToken.transfer(to, id);
 
-        assertEq(0, pageToken.balanceOf(from, id));
-        assertEq(1, pageToken.balanceOf(to, id));
+        if (!toIsUnsafe) {
+            assertEq(0, pageToken.balanceOf(from, id));
+            assertEq(1, pageToken.balanceOf(to, id));
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -191,6 +195,275 @@ contract PageTokenTest is Test {
 
         for (uint256 i = 0; i < accounts.length; ) {
             assertEq(0, pageToken.balanceOf(address(this), ids[i]));
+            assertEq(1, pageToken.balanceOf(to[i], ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             transferFrom
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotTransferFromWrongFrom() external {
+        address from = accounts[0];
+        address to = accounts[1];
+        uint256 id = 1;
+
+        assertEq(0, pageToken.balanceOf(from, id));
+
+        vm.expectRevert(PageToken.WrongFrom.selector);
+
+        pageToken.transferFrom(from, to, id);
+    }
+
+    function testCannotTransferFromUnsafeRecipient() external {
+        address from = accounts[0];
+        address to = address(0);
+        uint256 id = 1;
+
+        pageToken.setOwnerOf(id, from);
+
+        assertEq(1, pageToken.balanceOf(from, id));
+
+        vm.expectRevert(PageToken.UnsafeRecipient.selector);
+
+        pageToken.transferFrom(from, to, id);
+    }
+
+    function testCannotTransferFromNotAuthorized() external {
+        address from = accounts[0];
+        address to = accounts[1];
+        uint256 id = 1;
+
+        pageToken.setOwnerOf(id, from);
+
+        assertEq(1, pageToken.balanceOf(from, id));
+        assertTrue(!pageToken.isApprovedForAll(from, address(this)));
+
+        vm.expectRevert(PageToken.NotAuthorized.selector);
+
+        pageToken.transferFrom(from, to, id);
+    }
+
+    function testTransferFromSelf() external {
+        address from = address(this);
+        address to = accounts[1];
+        uint256 id = 1;
+
+        pageToken.setOwnerOf(id, from);
+
+        assertEq(1, pageToken.balanceOf(from, id));
+        assertEq(0, pageToken.balanceOf(to, id));
+
+        pageToken.transferFrom(from, to, id);
+
+        assertEq(0, pageToken.balanceOf(from, id));
+        assertEq(1, pageToken.balanceOf(to, id));
+    }
+
+    function testTransferFrom() external {
+        address from = accounts[0];
+        address to = accounts[1];
+        uint256 id = 1;
+
+        pageToken.setOwnerOf(id, from);
+
+        assertEq(1, pageToken.balanceOf(from, id));
+        assertEq(0, pageToken.balanceOf(to, id));
+
+        vm.prank(from);
+
+        pageToken.setApprovalForAll(address(this), true);
+
+        assertTrue(pageToken.isApprovedForAll(from, address(this)));
+        assertTrue(address(this) != from);
+
+        pageToken.transferFrom(from, to, id);
+
+        assertEq(0, pageToken.balanceOf(from, id));
+        assertEq(1, pageToken.balanceOf(to, id));
+    }
+
+    function testTransferFromFuzz(
+        uint256 id,
+        address from,
+        address to,
+        bool selfTransfer
+    ) external {
+        vm.assume(from != address(0));
+        vm.assume(from != to);
+
+        pageToken.setOwnerOf(id, from);
+
+        assertEq(1, pageToken.balanceOf(from, id));
+        assertEq(0, pageToken.balanceOf(to, id));
+
+        vm.prank(from);
+
+        if (!selfTransfer) {
+            pageToken.setApprovalForAll(address(this), true);
+
+            assertTrue(pageToken.isApprovedForAll(from, address(this)));
+            assertTrue(address(this) != from);
+        }
+
+        bool toIsUnsafe = to == address(0);
+
+        if (toIsUnsafe) vm.expectRevert(PageToken.UnsafeRecipient.selector);
+
+        pageToken.transferFrom(from, to, id);
+
+        if (!toIsUnsafe) {
+            assertEq(0, pageToken.balanceOf(from, id));
+            assertEq(1, pageToken.balanceOf(to, id));
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             batchTransferFrom
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotBatchTransferFromNotAuthorized() external {
+        address from = accounts[0];
+        address[] memory to = new address[](1);
+        uint256[] memory ids = new uint256[](1);
+        to[0] = accounts[1];
+        ids[0] = 1;
+
+        assertTrue(!pageToken.isApprovedForAll(from, address(this)));
+        assertTrue(from != address(this));
+
+        vm.expectRevert(PageToken.NotAuthorized.selector);
+
+        pageToken.batchTransferFrom(from, to, ids);
+    }
+
+    function testCannotBatchTransferFromWrongFromSelf() external {
+        address from = accounts[0];
+        address[] memory to = new address[](1);
+        uint256[] memory ids = new uint256[](1);
+        to[0] = accounts[1];
+        ids[0] = 1;
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertEq(0, pageToken.balanceOf(from, ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(from);
+        vm.expectRevert(PageToken.WrongFrom.selector);
+
+        pageToken.batchTransferFrom(from, to, ids);
+    }
+
+    function testCannotBatchTransferFromWrongFrom() external {
+        address from = accounts[0];
+        address[] memory to = new address[](1);
+        uint256[] memory ids = new uint256[](1);
+        to[0] = accounts[1];
+        ids[0] = 1;
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertEq(0, pageToken.balanceOf(from, ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(from);
+
+        pageToken.setApprovalForAll(address(this), true);
+
+        assertTrue(pageToken.isApprovedForAll(from, address(this)));
+
+        vm.expectRevert(PageToken.WrongFrom.selector);
+
+        pageToken.batchTransferFrom(from, to, ids);
+    }
+
+    function testCannotBatchTransferFromUnsafeRecipientSelf() external {
+        address from = accounts[0];
+        address[] memory to = new address[](1);
+        uint256[] memory ids = new uint256[](1);
+        to[0] = address(0);
+        ids[0] = 1;
+
+        for (uint256 i = 0; i < ids.length; ) {
+            pageToken.setOwnerOf(ids[i], from);
+
+            assertEq(1, pageToken.balanceOf(from, ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(from);
+        vm.expectRevert(PageToken.UnsafeRecipient.selector);
+
+        pageToken.batchTransferFrom(from, to, ids);
+    }
+
+    function testCannotBatchTransferFromUnsafeRecipient() external {
+        address from = accounts[0];
+        address[] memory to = new address[](1);
+        uint256[] memory ids = new uint256[](1);
+        to[0] = address(0);
+        ids[0] = 1;
+
+        for (uint256 i = 0; i < ids.length; ) {
+            pageToken.setOwnerOf(ids[i], from);
+
+            assertEq(1, pageToken.balanceOf(from, ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(from);
+
+        pageToken.setApprovalForAll(address(this), true);
+
+        assertTrue(pageToken.isApprovedForAll(from, address(this)));
+
+        vm.expectRevert(PageToken.UnsafeRecipient.selector);
+
+        pageToken.batchTransferFrom(from, to, ids);
+    }
+
+    function testBatchTransferFromSelf() external {
+        address from = address(1);
+        address[] memory to = new address[](accounts.length);
+        uint256[] memory ids = new uint256[](accounts.length);
+
+        for (uint256 i = 0; i < accounts.length; ) {
+            to[i] = accounts[i];
+            ids[i] = i;
+
+            pageToken.setOwnerOf(ids[i], from);
+
+            assertEq(1, pageToken.balanceOf(from, ids[i]));
+            assertEq(0, pageToken.balanceOf(to[i], ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(from);
+
+        pageToken.batchTransferFrom(from, to, ids);
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertEq(0, pageToken.balanceOf(from, ids[i]));
             assertEq(1, pageToken.balanceOf(to[i], ids[i]));
 
             unchecked {
