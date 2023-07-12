4dbf8711b33ffffdbf05ff7c231e0399c8a0490f
kphed <kpcrypto69@gmail.com>
2023-07-12 11:21:43 -0400

    Add updated FrontPage tests

--- test/FrontPage.t.sol
+++ test/FrontPage.t.sol
@@ -11,6 +11,7 @@ import {Page} from "src/Page.sol";
 import {TestERC721} from "test/lib/TestERC721.sol";
 
 contract FrontPageTests is Test, ERC721TokenReceiver {
+    bytes32 internal constant STORAGE_SLOT_NEXT_ID = bytes32(uint256(7));
     bytes32 internal constant SALT = keccak256("SALT");
     string internal constant NAME = "Test";
     string internal constant SYMBOL = "TEST";
@@ -24,7 +25,39 @@ contract FrontPageTests is Test, ERC721TokenReceiver {
 
     address[] internal accounts = [address(1), address(2), address(3)];
 
+    event Mint();
+    event BatchMint();
+
     receive() external payable {}
+
+    function _batchMint(
+        address to,
+        uint256 quantity
+    ) internal returns (uint256[] memory ids) {
+        uint256 msgValue = MINT_PRICE * quantity;
+        uint256 nextId = page.nextId();
+        ids = new uint256[](quantity);
+
+        vm.deal(to, msgValue);
+        vm.prank(to);
+        vm.expectEmit(false, false, false, true, address(page));
+
+        emit BatchMint();
+
+        page.batchMint{value: msgValue}(quantity);
+
+        uint256 finalNextId = nextId + quantity;
+
+        for (uint256 i = nextId; i < finalNextId; ) {
+            ids[i] = i;
+
+            assertEq(to, page.ownerOf(i));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
 
     constructor() {
         (uint256 collectionVersion, ) = book.upgradeCollection(
@@ -135,5 +168,246 @@ contract FrontPageTests is Test, ERC721TokenReceiver {
             creatorBalanceBefore + mintProceeds,
             address(msgSender).balance
         );
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             mint
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotMintSoldout() external {
+        address msgSender = address(this);
+
+        vm.store(address(page), STORAGE_SLOT_NEXT_ID, bytes32(MAX_SUPPLY + 1));
+
+        assertGt(page.nextId(), page.maxSupply());
+
+        vm.prank(msgSender);
+        vm.expectRevert(FrontPage.Soldout.selector);
+
+        page.mint();
+    }
+
+    function testCannotMintInvalidMsgValue(uint96 msgValue) external {
+        address msgSender = address(this);
+
+        vm.assume(msgValue != MINT_PRICE);
+        vm.deal(msgSender, msgValue);
+        vm.prank(msgSender);
+        vm.expectRevert(FrontPage.InvalidMsgValue.selector);
+
+        page.mint{value: msgValue}();
+    }
+
+    function testMint(address msgSender) external {
+        vm.assume(msgSender != address(0));
+
+        assertLe(page.nextId(), page.maxSupply());
+
+        uint256 nextId = page.nextId();
+        uint256 pageBalanceBefore = address(page).balance;
+
+        assertEq(address(0), page.ownerOf(nextId));
+
+        uint256 value = MINT_PRICE;
+
+        vm.deal(msgSender, MINT_PRICE);
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(page));
+
+        emit Mint();
+
+        page.mint{value: value}();
+
+        assertEq(msgSender, page.ownerOf(nextId));
+        assertEq(pageBalanceBefore + value, address(page).balance);
+        assertEq(nextId + 1, page.nextId());
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             batchMint
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotBatchMintInvalidMsgValue(uint256 quantity) external {
+        vm.assume(quantity != 0);
+        vm.assume(quantity <= MAX_SUPPLY);
+
+        address msgSender = address(this);
+        uint256 value = (quantity * MINT_PRICE) - 1;
+
+        vm.prank(msgSender);
+        vm.expectRevert(FrontPage.InvalidMsgValue.selector);
+
+        page.batchMint{value: value}(quantity);
+    }
+
+    function testCannotBatchMintSoldout(uint48 quantity) external {
+        vm.assume(quantity != 0);
+        vm.store(address(page), STORAGE_SLOT_NEXT_ID, bytes32(MAX_SUPPLY));
+
+        address msgSender = address(this);
+        uint256 value = uint256(quantity) * MINT_PRICE;
+
+        vm.deal(msgSender, value);
+        vm.expectRevert(FrontPage.Soldout.selector);
+
+        page.batchMint{value: value}(quantity);
+    }
+
+    function testBatchMint() external {
+        address msgSender = address(this);
+        uint256 quantity = 5;
+        uint256 startingId = page.nextId();
+
+        for (uint256 i = 0; i < quantity; ) {
+            assertEq(address(0), page.ownerOf(startingId + i));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        assertLe(page.nextId() + quantity, page.maxSupply());
+
+        uint256 value = quantity * MINT_PRICE;
+
+        vm.deal(msgSender, value);
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(page));
+
+        emit BatchMint();
+
+        page.batchMint{value: value}(quantity);
+
+        for (uint256 i = 0; i < quantity; ) {
+            assertEq(msgSender, page.ownerOf(startingId + i));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function testBatchMintFuzz(uint8 quantity) external {
+        address msgSender = address(this);
+        uint256 startingId = page.nextId();
+
+        for (uint256 i = 0; i < quantity; ) {
+            assertEq(address(0), page.ownerOf(startingId + i));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        assertLe(page.nextId() + quantity, page.maxSupply());
+
+        uint256 value = quantity * MINT_PRICE;
+
+        vm.deal(msgSender, value);
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(page));
+
+        emit BatchMint();
+
+        page.batchMint{value: value}(quantity);
+
+        uint256 endingId = startingId + quantity;
+
+        for (uint256 i = startingId; i < endingId; ) {
+            assertEq(msgSender, page.ownerOf(i));
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             redeem
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotRedeemUnauthorized() external {
+        address msgSender = address(this);
+        uint256 id = page.nextId();
+
+        assertEq(address(0), page.ownerOf(id));
+
+        vm.prank(msgSender);
+        vm.expectRevert(Page.Unauthorized.selector);
+
+        page.redeem(id);
+    }
+
+    function testRedeem() external {
+        address msgSender = address(this);
+        uint256 id = page.nextId();
+
+        assertEq(address(0), page.ownerOf(id));
+
+        page.mint{value: MINT_PRICE}();
+
+        assertEq(msgSender, page.ownerOf(id));
+
+        vm.expectRevert(ERC721.TokenDoesNotExist.selector);
+
+        collection.ownerOf(id);
+
+        page.redeem(id);
+
+        assertEq(address(0), page.ownerOf(id));
+        assertEq(msgSender, collection.ownerOf(id));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             batchRedeem
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotBatchRedeemUnauthorized() external {
+        address owner = address(this);
+        uint256 mintQuantity = 5;
+        address unauthorizedMsgSender = accounts[0];
+        uint256[] memory ids = _batchMint(owner, mintQuantity);
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertTrue(unauthorizedMsgSender != page.ownerOf(ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        vm.prank(unauthorizedMsgSender);
+        vm.expectRevert(Page.Unauthorized.selector);
+
+        page.batchRedeem(ids);
+    }
+
+    function testBatchRedeem() external {
+        address msgSender = address(this);
+        uint256 mintQuantity = 5;
+        uint256[] memory ids = _batchMint(msgSender, mintQuantity);
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertEq(msgSender, page.ownerOf(ids[i]));
+
+            vm.expectRevert(ERC721.TokenDoesNotExist.selector);
+
+            collection.ownerOf(ids[i]);
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        page.batchRedeem(ids);
+
+        for (uint256 i = 0; i < ids.length; ) {
+            assertEq(address(0), page.ownerOf(ids[i]));
+            assertEq(msgSender, collection.ownerOf(ids[i]));
+
+            unchecked {
+                ++i;
+            }
+        }
     }
 }
